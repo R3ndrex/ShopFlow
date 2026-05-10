650df06ce10f15a73af6ad6e5b0b98f16715df36
R3ndrex <yacholohchtoli@gmail.com>
2026-05-10 18:06:41 +0300

    simplify logic

--- server/src/services/categoryService.ts
+++ server/src/services/categoryService.ts
@@ -12,23 +12,11 @@ class CategoryService {
             name: category.name,
         }));
     }
-    async deleteCategory(name: string) {
-        const deletedCategory = await prisma.$transaction(async (tx) => {
-            const category = await tx.category.findFirst({ where: { name } });
-            if (!category) {
-                throw ApiError.badRequest("Category not found");
-            }
-            await tx.product.updateMany({
-                where: {
-                    categoryId: category.id,
-                },
-                data: {
-                    categoryId: null,
-                },
-            });
-            return await tx.category.delete({ where: { id: category.id } });
-        });
 
+    async deleteCategory(id: string) {
+        const deletedCategory = await prisma.category.delete({
+            where: { id: id },
+        });
         return { id: deletedCategory.id, name: deletedCategory.name };
     }
     async createCategory(name: string) {
