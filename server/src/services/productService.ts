a327456f1dd3c7fce1664d659f22fd8da3daef6a
R3ndrex <yacholohchtoli@gmail.com>
2026-05-10 18:09:57 +0300

    add create product, remove .? when not needed

--- server/src/services/productService.ts
+++ server/src/services/productService.ts
@@ -1,6 +1,11 @@
 import ApiError from "../error/ApiError.js";
+import type { ProductInfo } from "../generated/prisma/client.js";
 import { prisma } from "../lib/prisma.js";
-import { getDefaultVariant, getRating } from "../mappers/product.mapper.js";
+import {
+    getDefaultVariant,
+    getRating,
+    type Product,
+} from "../mappers/product.mapper.js";
 
 class ProductService {
     async getAllProducts() {
@@ -23,7 +28,6 @@ class ProductService {
             },
         });
         if (products.length <= 0) {
-            console.log(2);
             throw ApiError.notFound("Products not found");
         }
         return products.map((product) => {
@@ -39,11 +43,100 @@ class ProductService {
             };
         });
     }
+    async createProduct({
+        name,
+        slug,
+        variants,
+        categoryId,
+        productInfo,
+    }: {
+        name: string;
+        slug: string;
+        variants: Product["variants"];
+        categoryId: string;
+        productInfo: ProductInfo[];
+    }) {
+        const createdProduct = await prisma.product.create({
+            data: {
+                name,
+                slug,
+                productInfo: {
+                    createMany: { data: productInfo },
+                },
+                variants: {
+                    create: variants.map((v) => {
+                        return {
+                            isDefault: v.isDefault,
+                            color: {
+                                connect: {
+                                    id: v.colorId,
+                                },
+                            },
+                            size: {
+                                connect: {
+                                    id: v.sizeId,
+                                },
+                            },
+                            price: v.price,
+                            stock: v.stock,
+                            images: {
+                                createMany: { data: v.images },
+                            },
+                        };
+                    }),
+                },
+                category: {
+                    connect: {
+                        id: categoryId,
+                    },
+                },
+            },
+            include: {
+                variants: {
+                    include: {
+                        color: true,
+                        size: true,
+                        images: {
+                            omit: {
+                                variantId: true,
+                            },
+                        },
+                    },
+                },
+                productInfo: {
+                    omit: {
+                        productId: true,
+                    },
+                },
+                category: true,
+                ratings: {
+                    omit: {
+                        productId: true,
+                    },
+                },
+            },
+        });
+        const defaultVariant = getDefaultVariant(createdProduct);
+        const rating = getRating(createdProduct);
+        return {
+            id: createdProduct.id,
+            name: createdProduct.name,
+            category: createdProduct.category?.name,
+            images: defaultVariant.images.map((i) => i.url) || [],
+            price: defaultVariant.price,
+            size: defaultVariant.size,
+            color: defaultVariant.color,
+            stock: defaultVariant.stock || 0,
+            slug: createdProduct.slug,
+            rating,
+            description: createdProduct.productInfo,
+        };
+    }
     async getProduct(id: string, slug: string) {
         const product = await prisma.product.findFirst({
             where: {
-                id: id,
-                slug: slug,
+                id,
+                slug,
             },
             include: {
                 variants: {
@@ -78,18 +171,17 @@ class ProductService {
         return {
             id: product.id,
             name: product.name,
-            category: product?.category.name,
-            images: defaultVariant?.images.map((i) => i.url) || [],
-            price: defaultVariant?.price,
-            size: defaultVariant?.size,
-            color: defaultVariant?.color,
-            stock: defaultVariant?.stock || 0,
+            category: product.category?.name,
+            images: defaultVariant.images.map((i) => i.url) || [],
+            price: defaultVariant.price,
+            size: defaultVariant.size,
+            color: defaultVariant.color,
+            stock: defaultVariant.stock || 0,
             slug: product.slug,
             rating,
-            description: product?.productInfo,
+            description: product.productInfo,
         };
     }
-    async createProduct() {}
     async deleteProduct(id: string, slug: string) {
         const deletedProduct = await prisma.product.delete({
             where: {
@@ -129,15 +221,15 @@ class ProductService {
         return {
             id: deletedProduct.id,
             name: deletedProduct.name,
-            category: deletedProduct?.category.name,
-            images: defaultVariant?.images.map((i) => i.url) || [],
-            price: defaultVariant?.price,
-            size: defaultVariant?.size,
-            color: defaultVariant?.color,
-            stock: defaultVariant?.stock || 0,
+            category: deletedProduct.category?.name,
+            images: defaultVariant.images.map((i) => i.url) || [],
+            price: defaultVariant.price,
+            size: defaultVariant.size,
+            color: defaultVariant.color,
+            stock: defaultVariant.stock || 0,
             slug: deletedProduct.slug,
             rating,
-            description: deletedProduct?.productInfo,
+            description: deletedProduct.productInfo,
         };
     }
 }
