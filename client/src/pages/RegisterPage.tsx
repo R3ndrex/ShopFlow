6f79d0fb77fe322757cd2ef45de3f184dad963a1
R3ndrex <yacholohchtoli@gmail.com>
2026-05-10 18:13:44 +0300

    fix tailwind errors

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,37 +1,37 @@
 import { NavLink } from "react-router-dom";
 export function RegisterPage() {
     return (
-        <main className="mb-[5rem] flex flex-col items-center ">
+        <main className="mb-20 flex flex-col items-center ">
             <form
-                className="flex flex-col gap-[0.5em] m-[1rem] auth-form bg-[var(--color-surface)] p-[1rem] rounded-[var(--radius-lg)]"
+                className="flex flex-col gap-[0.5em] m-4 auth-form bg-(--color-surface) p-4 rounded-lg"
                 action=""
                 method="post"
             >
                 <h1 className=" text-center ">Register</h1>
                 <label htmlFor="username">Username</label>
                 <input
-                    className="p-[0.5em] bg-[var(--color-bg)] outline-0 text-[var(--color-text)] rounded-[var(--radius-md)]"
+                    className="p-[0.5em] bg-(--color-bg) outline-0 text-(--color-text) rounded-md"
                     type="text"
                     name="username"
                     id="username"
                 />
                 <label htmlFor="email">Email</label>
                 <input
-                    className="p-[0.5em] bg-[var(--color-bg)] outline-0 text-[var(--color-text)] rounded-[var(--radius-md)]"
+                    className="p-[0.5em] bg-(--color-bg) outline-0 text-(--color-text) rounded-md"
                     type="email"
                     name="email"
                     id="email"
                 />
                 <label htmlFor="password">Password</label>
                 <input
-                    className="p-[0.5em] bg-[var(--color-bg)] outline-0 text-[var(--color-text)] rounded-[var(--radius-md)]"
+                    className="p-[0.5em] bg-(--color-bg) outline-0 text-(--color-text) rounded-md"
                     type="password"
                     name="password"
                     id="password"
                 />
                 <label htmlFor="confirmPassword">Confirm Password</label>
                 <input
-                    className="p-[0.5em] bg-[var(--color-bg)] outline-0 text-[var(--color-text)] rounded-[var(--radius-md)]"
+                    className="p-[0.5em] bg-(--color-bg) outline-0 text-(--color-text) rounded-md"
                     type="password"
                     name="confirmPassword"
                     id="confirmPassword"
@@ -46,7 +46,7 @@ export function RegisterPage() {
                     Already have one?{" "}
                     <NavLink
                         className={
-                            "border-b-[var(--color-accent)] border-bottom-animation"
+                            "border-b-(--color-accent) border-bottom-animation"
                         }
                         to={"/login"}
                     >
