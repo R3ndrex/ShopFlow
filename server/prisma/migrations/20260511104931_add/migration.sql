f388c911e111013053930479c153dc97e3abd1f0
R3ndrex <yacholohchtoli@gmail.com>
2026-05-11 13:59:14 +0300

    add indexe name to category, color, size

---
+++ server/prisma/migrations/20260511104931_add/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "Category_name_idx" ON "Category"("name");
+
+-- CreateIndex
+CREATE INDEX "Color_name_idx" ON "Color"("name");
+
+-- CreateIndex
+CREATE INDEX "Size_name_idx" ON "Size"("name");
